d22b328f6fa95c9dc1a6179bb594fd25d045f3e4
Michael John Decker <mdecke@bgsu.edu>
2018-09-07 15:09:12 -0400

    Add my name is program for lab 1

---
+++ labs/my_name_is.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file my_name_is.cpp
+ *
+ * Prints my name.
+ *
+ * @author Michael John Decker, Ph.D. <mdecke@bsgu.edu>
+ */
+
+#include <iostream>
+
+int main(int argc, char * argv[]) {
+
+  std::cout << "My name is: Michael John Decker, Ph.D.\n";
+
+  return 0;
+}
