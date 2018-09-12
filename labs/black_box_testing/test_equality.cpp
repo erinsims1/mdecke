8b4a8e0ac5aee710843b5e192b92c63f6b141d38
Michael John Decker <mdecke@bgsu.edu>
2018-09-12 18:23:17 -0400

    Add string

---
+++ labs/black_box_testing/test_equality.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file test_default_ctor.cpp
+ *
+ * Tests String class default constructor.
+ *
+ * @author Michael John Decker, Ph.D. <mdecke@bsgu.edu>
+ */
+
+#include "string.hpp"
+
+#include <cassert>
+#include <iostream>
+
+int main () {
+
+  int a;
+
+    return 0;
+}
