e4b1ee8c632229b97f11c687113ddeed8fe46b13
Michael John Decker <mdecke@bgsu.edu>
2018-09-12 15:49:42 -0400

    Add first test for concatenate

--- example/string/test_concat.cpp
+++ example/string/test_concat.cpp
@@ -7,12 +7,27 @@
  */
 
 #include "string.hpp"
-
 #include <cassert>
+
+
 #include <iostream>
 
 //===========================================================================
 int main () {
+
+
+    // setup
+    String lhs;
+    String rhs("");
+
+    // test
+    String result = lhs + rhs;   
+    
+    // verification
+    assert(lhs == "");
+    assert(rhs == "");
+    assert(result == "");
+    assert(result.length() == 0);
 
     return 0;
 }
